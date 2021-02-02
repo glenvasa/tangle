18758d46fd2e145f4da40c264262a77abf2925eb
glenvasa <70590043+glenvasa@users.noreply.github.com>
2021-02-01 19:51:53 -0500

    imported ApiService and added getProducts fn to load all products on first render

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from 'src/app/services/api.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
+  items: any[];
 
-  constructor() { }
+  constructor(private api: ApiService) {}
 
   ngOnInit(): void {
+    // run to get all products upon loading
+    this.getProducts();
   }
 
+  getProducts() {
+    // subscribe to an observable to get back results
+    this.api.getJSON().subscribe((resp) => {
+      this.items = resp;
+    });
+  }
 }
