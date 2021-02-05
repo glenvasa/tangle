1581613e5be2efca221e12802615a57a57642af6
glenvasa <70590043+glenvasa@users.noreply.github.com>
2021-02-04 19:43:46 -0500

    Create checkout-products.component.ts

---
+++ src/app/components/checkout-products/checkout-products.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { ShoppingCartService } from 'src/app/services/shopping-cart.service';
+
+@Component({
+  selector: 'app-checkout-products',
+  templateUrl: './checkout-products.component.html',
+  styleUrls: ['./checkout-products.component.css'],
+})
+export class CheckoutProductsComponent implements OnInit {
+  @Input() checkout_products: any[];
+
+  constructor(public shopping_cart_service: ShoppingCartService) {}
+
+  ngOnInit(): void {
+    console.log('products ', this.checkout_products);
+  }
+}
