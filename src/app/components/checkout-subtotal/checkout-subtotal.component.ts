193c90e5b67d95b806f22ca368f2fc380a3fa10b
glenvasa <70590043+glenvasa@users.noreply.github.com>
2021-02-05 19:35:35 -0500

    imported ShoppingCartService to constructor

--- src/app/components/checkout-subtotal/checkout-subtotal.component.ts
+++ src/app/components/checkout-subtotal/checkout-subtotal.component.ts
@@ -1,15 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { ShoppingCartService } from 'src/app/services/shopping-cart.service';
 
 @Component({
   selector: 'app-checkout-subtotal',
   templateUrl: './checkout-subtotal.component.html',
-  styleUrls: ['./checkout-subtotal.component.css']
+  styleUrls: ['./checkout-subtotal.component.css'],
 })
 export class CheckoutSubtotalComponent implements OnInit {
+  constructor(public shoppingCart: ShoppingCartService) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
